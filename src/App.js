af6765d0b591a050f01ac06ad9b65cc4a4a7edce
Anton <klimantow@yandex.ru>
2019-07-24 10:02:53 +0300

    add styled-components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,7 @@
-import React from 'react';
+import React from "react"
 
 function App() {
-  return (
-    <div>
-test
-    </div>
-  );
+  return <div>test</div>
 }
 
-export default App;
+export default App
